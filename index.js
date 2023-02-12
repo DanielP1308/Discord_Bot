39ea65a68a74b703ccb7845e0249f7f39276179e
DanielP1308 <65129846+DanielP1308@users.noreply.github.com>
2023-02-12 15:53:12 +0000

    Update to Index.js
    Deleted Ghibli API.
    Added search.
    Correct URL to MAL.

--- index.js
+++ index.js
@@ -6,32 +6,8 @@ const { XMLHttpRequest } = require('xhr2');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-var ghibliObject = [];
 var animeObject = new Array();
-var arrlen = 0;
-var keysGhibli = [];
 var keysSearchAnime = [];
-
-
-function getGhibli() {
-  var request = new XMLHttpRequest();
-  request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-
-  request.onload = function () {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response);
-    //const obj = data;
-    data.forEach(movie => {
-    // Log each movie's title
-      ghibliObject.push(movie);
-      arrlen++;
-    });
-    keysGhibli = Object.keys(ghibliObject);
-  };
-  // Send request
-  request.send();
-  return ghibliObject;
-}
 
 async function searchAnime(search) {
   var animeData = [];
@@ -48,32 +24,17 @@ async function searchAnime(search) {
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
-    ghibliObject = getGhibli();
+    //ghibliObject = getGhibli();
 });
 
 client.on('messageCreate', function(msg){
   console.log("Message Received!")
-    if(msg.content === 'ghibli' || msg.content === 'g'){
-        var getRandom = Math.floor(Math.random() * arrlen);
-        const embededMsg = new EmbedBuilder()
-          .setColor(0x0099FF)
-          .setTitle(ghibliObject[keysGhibli[getRandom]].title)
-          .setURL(`https://myanimelist.net/search/all?cat=all&q=${ghibliObject[keysGhibli[getRandom]].title.replaceAll(' ', '%20')}`)
-          .setDescription(ghibliObject[keysGhibli[getRandom]].description)
-          .setThumbnail(ghibliObject[keysGhibli[getRandom]].image)
-          .addFields(
-            { name: 'Director', value: ghibliObject[keysGhibli[getRandom]].director, inline: true },
-            { name: 'Producer', value: ghibliObject[keysGhibli[getRandom]].producer, inline: true },
-            { name: 'Release Date', value: ghibliObject[keysGhibli[getRandom]].release_date, inline: true }
-          )
-          .setImage(ghibliObject[keysGhibli[getRandom]].movie_banner)
-          .setTimestamp()
-          .setFooter({ text: 'DanielP1308', iconURL:  ghibliObject[keysGhibli[getRandom]].image });
-        msg.reply({ embeds: [embededMsg] });
-        console.log(ghibliObject[keysGhibli[getRandom]].title);
-    }
-    if(msg.content === 'a') {
-      animeObject = searchAnime('naruto');
+    var command =  msg.content.substring(0, 2)
+
+    if(command === '-a') {
+      var search = msg.content.substring(3)
+      animeObject = searchAnime(search);
+      var urlCorrect = search.replace(/ /g, '_')
       var embededMsg = new EmbedBuilder();
       var count = 0;
       
@@ -82,8 +43,8 @@ client.on('messageCreate', function(msg){
         
         embededMsg
         .setColor(0x0099FF)
-        .setTitle("Search String")
-        .setURL(res[keysSearchAnime[0]].url)
+        .setTitle(search)
+        .setURL(`https://myanimelist.net/search/all?q=${ urlCorrect }&cat=all`)
         /*.setDescription(list[keysSearchAnime[0]].synopsis)
         .setThumbnail(list[keysSearchAnime[0]].images.jpg.small_image_url)
         .setImage(list[keysSearchAnime[0]].images.jpg.image_url)
